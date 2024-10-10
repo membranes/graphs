7fd32533daf6969d43c0753e619783e533f63b14
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-10 12:15:12 +0100

    Update docs/source/_static/js/pages/details.js

--- docs/source/_static/js/pages/details.js
+++ docs/source/_static/js/pages/details.js
@@ -79,7 +79,7 @@ jQuery.getJSON(url, function (source){
             align: 'left'
         },
         subtitle: {
-            text: '<a href="https://github.com/orgs/prml-0004/projects/2/views/3"><img alt="Static Badge" src="https://img.shields.io/badge/project-backlog-green" style="margin-left: 5px"></a>',
+            text: '<a href="https://github.com/orgs/prml-0004/projects/2/views/3">Project Details Log</a>',
             align: 'left'
         },
         plotOptions: {
