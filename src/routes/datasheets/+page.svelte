51d56ff5b2565208b695b07e6ede3b58bcd2b469
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-17 20:37:54 +0000

    Update src/routes/datasheets/+page.svelte

--- src/routes/datasheets/+page.svelte
+++ src/routes/datasheets/+page.svelte
@@ -5,10 +5,6 @@
 <h1>datasheets</h1>
 
 <ul>
-	<li><a href="/datasheets/motivation">motivation</a></li>
-	<li><a href="/datasheets/collection">collection mechanism</a></li>
-	<li><a href="/datasheets/features">data features</a></li>
-    <li><a href="/datasheets/statements">nlp data statements</a></li>
 	{#each data.summaries as { slug, title}}
 		<li><a href="/datasheets/{slug}">{title}</a></li>
 	{/each}
