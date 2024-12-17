e82f3cc879b390a695b8c502771a1bc920c98d18
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-17 18:05:24 +0000

    Add src/routes/datasheets/+page.svelte

---
+++ src/routes/datasheets/+page.svelte
@@ -0,0 +1,8 @@
+<h1>datasheets</h1>
+
+<ul>
+	<li><a href="/datasheets/motivation">motivation</a></li>
+	<li><a href="/datasheets/collection">collection mechanism</a></li>
+	<li><a href="/datasheets/features">data features</a></li>
+    <li><a href="/datasheets/statements">nlp data statements</a></li>
+</ul>
