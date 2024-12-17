9d9eb24f967583ee9527ef8a0c20d2525c438e21
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-17 20:36:19 +0000

    Update src/routes/datasheets/+page.svelte

--- src/routes/datasheets/+page.svelte
+++ src/routes/datasheets/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
 <h1>datasheets</h1>
 
 <ul>
@@ -5,4 +9,7 @@
 	<li><a href="/datasheets/collection">collection mechanism</a></li>
 	<li><a href="/datasheets/features">data features</a></li>
     <li><a href="/datasheets/statements">nlp data statements</a></li>
+	{#each data.summaries as { slug, title}}
+		<li><a href="/datasheets/{slug}">{title}</a></li>
+	{/each}
 </ul>
