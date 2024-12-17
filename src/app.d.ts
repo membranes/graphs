fdf772cc3bcf8d6a8cfee9377f018325c7bf818d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-17 17:29:29 +0000

    Add src/app.d.ts

---
+++ src/app.d.ts
@@ -0,0 +1,15 @@
+/* 
+ * Refer to https://svelte.dev/docs/kit/types#app.d.ts
+ * for information about interfaces.
+ */
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
