3311914c6bd75b9bf46ea49ef6482c313317ef62
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-02 23:04:42 +0000

    Add src/routes/+layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Navigation from "$lib/Navigation.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import '../styles/global.css';
+</script>
+
+<div class="layout">
+    <Navigation />
+    <slot />
+    <Footer />
+</div>
