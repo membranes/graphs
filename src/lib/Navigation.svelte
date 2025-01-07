72afab722628fd4297fa5d993c66ba23c558b423
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-07 19:46:12 +0000

    Update src/lib/Navigation.svelte

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -2,6 +2,6 @@
     <div class="navigation">        
         <a href="/">home</a><br>
         <a href="/about">about</a><br>
-        <a href="/frames">frames</a>        
+        <a href="/frames">frames</a>
     </div>
 </nav>
