145f8219773908495a557a064f57ce6f84cfdd6d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-08 22:08:40 +0000

    Update assets/js/module/latest.js

--- assets/js/module/latest.js
+++ assets/js/module/latest.js
@@ -1,5 +1,18 @@
-import data from "../../../temporary/latest.json" with {type: 'json'};
 
-export function stamp() {
+/* 
+	import data from "../../../temporary/latest.json" assert {type: 'json'};
+
+	export function stamp() {
 		return data.time
+	} 
+*/
+
+export function stamp() { fetch("../../../temporary/latest.json")
+	.then((response) => response.json())
+	.then(data => {
+		return data.time;
+	})
+	.catch(error => {
+		console.error(error);
+	});
 }
