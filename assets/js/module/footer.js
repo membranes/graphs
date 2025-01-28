b62fbabef9acbf41a3a991965c5da490e98f3257
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-28 07:32:45 +0000

    Update assets/js/module/footer.js

--- assets/js/module/footer.js
+++ assets/js/module/footer.js
@@ -4,7 +4,7 @@ export function text() {
 	const datestr = new Date();
 
 	let panel;
-	panel = '<div class="footer">Artificial Intelligence Unit<br>' + `${datestr}` + '</div>'
+	panel = '<div class="by">Artificial Intelligence Unit<br>' + `${datestr}` + '</div>'
 
 	return panel
 
