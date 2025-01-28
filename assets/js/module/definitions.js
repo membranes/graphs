e8b83b52a41f952fc28e2b9e885e2a4e19667a55
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-28 10:30:10 +0000

    Update assets/js/module/definitions.js

--- assets/js/module/definitions.js
+++ assets/js/module/definitions.js
@@ -1,5 +1,25 @@
-import definition from 'temporary/definitions.json' with  {type: 'json'};
+import definitions from '../../../temporary/definitions.json' with  {type: 'json'};
 
-export function terms() {
-	return definition
+function instance(name, desc) {
+
+	return `<tr><td>${name}</td><td>${desc}</td></tr>`
+}
+
+export function vocabulary() {
+
+	var table = '<table>' +
+		'<colgroup>' +
+		'<col span="1" style="width: 26.5%;">' +
+		'<col span="1" style="width: 63.5%;">' +
+		'</colgroup>' +
+		' <thead><tr style="text-align: left"><th>term</th><th>definition</th></tr></thead>';
+
+	let end = '</table>';
+
+	for (let i = 0; i < definitions.length; i++) {
+		let next = instance(`${definitions[i].name}`, `${definitions[i].desc}`);
+		table = table + next;
+	}
+
+	return table + end
 }
