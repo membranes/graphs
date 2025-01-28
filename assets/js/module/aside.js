00d8f78316b765da6cb1fdc131b71c20bd1ececc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-28 00:57:49 +0000

    Update assets/js/module/aside.js

--- assets/js/module/aside.js
+++ assets/js/module/aside.js
@@ -2,14 +2,25 @@ import aside from "../../../temporary/aside.json" with { type:'json' };
 
 function hyperlink(string, name) {
 
-	return `<a href=${string} target="_blank">${name}</a>`
+	return `<a href=${string} target="_parent">${name}</a>`
+}
+
+function second(string, name) {
+
+	return `<a style="margin-left: 18px" href=${string} target="_parent">${name}</a>`
 }
 
 export function setup() {
 
 	let panel;
 	panel = hyperlink(`${aside.index}`, 'index') + `<br>` +
-		hyperlink(`${aside.background}`, 'background');
+		hyperlink(`${aside.background}`, 'background') + `<br>` +
+		hyperlink(`${aside.preliminaries}`, 'preliminaries') + `<br>` +
+		`Metrics<br>` +
+		second(`${aside.metrics.error}`, 'error') + `<br>` +
+		second(`${aside.metrics.financial}`, 'financial')+ `<br>` +
+		`Appendix<br>` +
+		second(`${aside.appendix.definitions}`, 'definitions');
 
 	return panel
 }
