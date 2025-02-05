b3eaa956f6c1a5a8fa5baa5fe62813142512470e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-05 21:46:24 +0000

    Update assets/js/module/by.js

--- assets/js/module/by.js
+++ assets/js/module/by.js
@@ -4,7 +4,7 @@ export function by() {
 	const datestr = new Date();
 
 	let text;
-	text = '<div class="by">Artificial Intelligence Unit<br>' + `${datestr}` + '</div>'
+	text = '<div class="by">Artificial Intelligence Unit<br>' + `${datestr.toUTCString()}` + '</div>'
 
 	return text
 
